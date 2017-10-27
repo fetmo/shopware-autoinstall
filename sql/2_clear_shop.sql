8467af10e7a229a7748137f2deb1b3df87c3cab1
Moritz Jung <moritz.jung@outlook.de>
2017-10-27 21:37:45 +0000

    Init

---
+++ sql/2_clear_shop.sql
@@ -0,0 +1,64 @@
+-- MySQL dump 10.13  Distrib 5.6.33, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: shopware
+-- ------------------------------------------------------
+-- Server version	5.6.33-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `s_core_shops`
+--
+
+DROP TABLE IF EXISTS `s_core_shops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `s_core_shops` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `main_id` int(11) unsigned DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `position` int(11) NOT NULL,
+  `host` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `base_path` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `base_url` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `hosts` text COLLATE utf8_unicode_ci NOT NULL,
+  `secure` int(1) unsigned NOT NULL,
+  `secure_host` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `secure_base_path` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `template_id` int(11) unsigned DEFAULT NULL,
+  `document_template_id` int(11) unsigned DEFAULT NULL,
+  `category_id` int(11) unsigned DEFAULT NULL,
+  `locale_id` int(11) unsigned DEFAULT NULL,
+  `currency_id` int(11) unsigned DEFAULT NULL,
+  `customer_group_id` int(11) unsigned DEFAULT NULL,
+  `fallback_id` int(11) unsigned DEFAULT NULL,
+  `customer_scope` int(1) NOT NULL,
+  `default` int(1) unsigned NOT NULL,
+  `active` int(1) NOT NULL,
+  `always_secure` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `main_id` (`main_id`),
+  KEY `host` (`host`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-27 20:51:06
---
+++ start.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+read -p 'Database host?     '  host
+read -p 'Database port?     '  port
+read -p 'Database name?     '  dbname
+read -p 'Database user?     '  user
+read -p 'Database password? '  password
+
+read -p 'Shop name?         '  shopname
+read -p 'Shop url?          '  url
+
+echo "" > sql/3_create_shop.sql
+echo "INSERT INTO 's_core_shops' ('id', 'main_id', 'name', 'title', 'position', 'host', 'base_path', 'base_url', 'hosts', 'secure', 'secure_host', 'secure_base_path', 'template_id', 'document_template_id', 'category_id', 'locale_id', 'currency_id', 'customer_group_id', 'fallback_id', 'customer_scope', 'default', 'active', 'always_secure') VALUES (1, NULL, '$shopname', '$shopname', 0, '$url', NULL, NULL, '$url', 0, NULL, NULL, 22, 22, 3, 1, 1, 1, NULL, 0, 1, 1, 0);" >> sql/3_create_shop.sql
+
+if [ ! -d shop ]
+then
+	mkdir shop
+fi
+
+tar xzf shop.tar.gz -C /shop
+
+for file in sql/*.sql; do
+	cat $file
+#	mysql -u$user -p$password -h$host -P$port $dbname < $file
+done
+
+cd shop
+
+if [ -e config.php ]
+then
+	echo "" > config.php
+else
+	touch config.php
+fi
+
+echo "<?php" >> config.php
+echo "return array(" >> config.php
+echo "  'db' => array(" >> config.php
+echo "    'username' => '$user'," >> config.php
+echo "    'password' => '$password'," >> config.php
+echo "    'dbname' => '$dbname'," >> config.php
+echo "    'host' => '$host'," >> config.php
+echo "    'port' => '$port'" >> config.php
+echo "  )" >> config.php
+echo ");" >> config.php
+
+if [ -d vendor ]
+	rm -rf vendor
+fi
+
+wget http://getcomposer.org/composer.phar
+chmod +x composer.phar
+
+php composer.phar install
+
+chmod +x bin/console
+php bin/console sw:cache:clear
+
+
+
+
+
+
